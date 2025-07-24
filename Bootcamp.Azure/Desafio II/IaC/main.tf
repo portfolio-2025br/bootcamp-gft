f4a6caa27425bb75f8fc61c1e1c8d06c15e029b5
Claudio André <portfolio-2025br@claudioandre.slmail.me>
2025-07-24 13:34:06 -0300

    feat: add criando máquinas virtuais na Azure
    O objetivo do projeto é conhecer e usar Azure GUI e CLI.
    
    Trilha Introdução à Cloud com Azure: desafio II.
    
    Signed-off-by: Claudio André <portfolio-2025br@claudioandre.slmail.me>

---
+++ Bootcamp.Azure/Desafio II/IaC/main.tf
@@ -0,0 +1,181 @@
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = "${random_pet.prefix.id}-rg"
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${random_pet.prefix.id}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "${random_pet.prefix.id}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "${random_pet.prefix.id}-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rules
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "${random_pet.prefix.id}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    #checkov:skip=CKV_AZURE_9:Máquina para testes IGNORAR o erro
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "web"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    #checkov:skip=CKV_AZURE_160:Máquina para testes IGNORAR o erro
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "${random_pet.prefix.id}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  #checkov:skip=CKV_AZURE_119:Máquina para testes IGNORAR o erro
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  # Restricting public access to these blobs is crucial to ensure data confidentiality and to prevent unauthorized access.
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled = false
+  min_tls_version      = "TLS1_2"
+  #checkov:skip=CKV2_AZURE_1:Máquina para testes IGNORAR o erro
+  #checkov:skip=CKV2_AZURE_33:Máquina para testes IGNORAR o erro
+  #checkov:skip=CKV2_AZURE_38:Máquina para testes IGNORAR o erro
+  #checkov:skip=CKV2_AZURE_40:Máquina para testes IGNORAR o erro
+  #checkov:skip=CKV2_AZURE_41:Máquina para testes IGNORAR o erro
+  #checkov:skip=CKV2_AZURE_47:Máquina para testes IGNORAR o erro
+  #checkov:skip=CKV_AZURE_206:Vixe, tá achando que eu sou rico?
+  # account_replication_type = "GRS"  # Or use RAGRS, GZRS, or RAGZRS as per your requirement.
+  queue_properties  {
+    logging {
+        delete                = true
+        read                  = true
+        write                 = true
+        version               = "1.0"
+        retention_policy_days = 10
+    }
+  }
+}
+
+
+# Create virtual machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  admin_username        = "azureuser"
+  admin_password        = random_password.password.result
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+  encryption_at_host_enabled=true
+  allow_extension_operations=false
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+  }
+}
+
+# Install IIS web server to the virtual machine
+resource "azurerm_virtual_machine_extension" "web_server_install" {
+  name                       = "${random_pet.prefix.id}-wsi"
+  virtual_machine_id         = azurerm_windows_virtual_machine.main.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
+    }
+  SETTINGS
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+resource "random_password" "password" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
+}
+
+resource "random_pet" "prefix" {
+  prefix = var.prefix
+  length = 1
+}
---
+++ Bootcamp.Azure/Desafio II/README.md
@@ -0,0 +1,31 @@
+# Criando Máquinas Virtuais na Azure
+
+Criei a máquina virtual usando a GUI e além disto, usando material disponibilizado em
+[Microsoft Learn](https://learn.microsoft.com/), iniciei uma jornada IaC (infraestrutura como código).
+
+Escaneei o código IaC para evitar problemas de configuração (naturalmente, devido aos custos, eu ignorei alguns erros).
+
+## Evidências Relevantes
+
+Veja uma abordagem inicial de [Infraestrutura como Código](IaC/) no Azure.
+
+### Outras Evidências:
+
+![Desafio I](images/Todos-Videos-I.png);
+
+****
+
+![Desafio II](images/Todos-Videos-II.png);
+
+****
+
+![Desafio III](images/Análise%20Estática.png);
+
+## Meu Endosso à Certificação AZ-900
+
+Concordo que a certificação AZ-900 é útil e de interesse para os profissionais de TI. Por esta razão eu me certifiquei:
+
+![Badges](https://images.credly.com/size/160x160/images/be8fcaeb-c769-4858-b567-ffaaa73ce8cf/image.png)
+
+[Veja todos meus badges neste link](https://www.credly.com/users/claudioandre-br).
+
